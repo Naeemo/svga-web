5174c6da942aca7ace8fdb2d8592608aab512250
naeemo <370552604@qq.com>
2021-04-12 18:05:57 +0800

    fix continue parsing when image decode encounter error(s).
    Signed-off-by: naeemo <370552604@qq.com>

--- src/parser/worker.ts
+++ src/parser/worker.ts
@@ -3,22 +3,19 @@ import VideoEntity, { AudioSources, ImageSources } from './video-entity'
 import { com } from '../proto/svga'
 import svga = com.opensource.svga
 
-/**
- * audio/x-mpeg
- * SUQz in base64
- * 0b01001001, 0b01000100, 0b00110011
- */
-const audioPrefix = Uint8Array.of(0b01001001)
-
-/**
- * image/png
- * iVBO in base64
- * 0b10001001, 0b01010000, 0b01001110
- */
-// const pngPrefix = Uint8Array.of(0b10001001)
-
-function isAudioData(data: Uint8Array): boolean {
-  return data[0] === audioPrefix[0]
+enum BinaryPrefix {
+  /**
+   * audio/x-mpeg
+   * SUQz in base64
+   * 0b01001001, 0b01000100, 0b00110011
+   */
+  AUDIO_XMPEG = 0b01001001,
+  /**
+   * image/png
+   * iVBO in base64
+   * 0b10001001, 0b01010000, 0b01001110
+   */
+  IMAGE_PNG = 0b10001001,
 }
 
 onmessage = function (event: MessageEvent<{ data: ArrayBuffer; id: number }>) {
@@ -62,17 +59,28 @@ onmessage = function (event: MessageEvent<{ data: ArrayBuffer; id: number }>) {
   for (const key in movie.images) {
     const uint8 = movie.images[key]
 
-    // skip audio data
-    if (isAudioData(uint8)) {
+    // skip audio data, no work needed here
+    if (uint8[0] === BinaryPrefix.AUDIO_XMPEG) {
+      continue
+    }
+
+    // only support png now
+    if (uint8[0] !== BinaryPrefix.IMAGE_PNG) {
+      console.warn('unrecognized svga image source', uint8, movie)
       continue
     }
 
     const blob = new Blob([uint8], { type: 'image/png' })
     promises.push(
-      createImageBitmap(blob).then((bitMap) => {
-        images[key] = bitMap
-        transferables.push(bitMap)
-      })
+      createImageBitmap(blob)
+        .then((bitMap) => {
+          images[key] = bitMap
+          transferables.push(bitMap)
+        })
+        .catch((err) => {
+          console.error('decode svga image source fail:\n', err)
+          console.error(uint8, blob, movie)
+        })
     )
   }
 
