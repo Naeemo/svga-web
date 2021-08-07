bfc546efc4d82f8635b9fce4b48bf877ce198d16
naeemo <370552604@qq.com>
2021-08-07 16:30:40 +0800

    update npm dependencies.
    Signed-off-by: naeemo <370552604@qq.com>

---
+++ scripts/generate-proto.mjs
@@ -0,0 +1,42 @@
+/**
+ * generate js and ts types from .proto file
+ * https://github.com/protobufjs/protobuf.js#pbts-for-typescript
+ */
+
+import pbjs from 'protobufjs/cli/pbjs.js'
+import pbts from 'protobufjs/cli/pbts.js'
+
+// generate js from proto file
+const generateJS = () => new Promise((resolve, reject) =>
+  pbjs.main(
+    [
+      '--target', 'static-module',
+      '-w', 'es6',
+      '-o', './src/proto/svga.js',
+      '--no-create',
+      '--no-encode',
+      '--no-verify',
+      '--no-convert',
+      '--no-beautify',
+      '--no-delimited',
+      './src/proto/svga.proto'
+    ],
+    (err) => (err ? reject(err) : resolve())
+  )
+)
+
+// generate types from generated js
+const generateTypes = () => new Promise((resolve, reject) =>
+  pbts.main(['-o', './src/proto/svga.d.ts', './src/proto/svga.js'], (err) =>
+    err ? reject(err) : resolve()
+  )
+)
+
+
+/**
+ * cli equivalent:
+ * pbjs -t static-module --es6 -o ../src/proto/svga.js --no-delimited ../src/proto/svga.proto
+ * pbts -o ../src/proto/svga.d.ts ../src/proto/svga.js
+ */
+await generateJS()
+await generateTypes()
