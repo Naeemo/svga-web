524db8648dad41d45ea082323172c429bd7cc388
Naeemo <naeemo@qq.com>
2026-04-12 18:43:00 +0800

    chore: update Node.js version and upgrade dependencies

--- scripts/generate-proto.mjs
+++ scripts/generate-proto.mjs
@@ -7,31 +7,35 @@ import pbjs from 'protobufjs-cli/pbjs.js'
 import pbts from 'protobufjs-cli/pbts.js'
 
 // generate js from proto file
-const generateJS = () => new Promise((resolve, reject) =>
-  pbjs.main(
-    [
-      '--target', 'static-module',
-      '-w', 'es6',
-      '-o', './src/proto/svga.js',
-      '--no-create',
-      '--no-encode',
-      '--no-verify',
-      '--no-convert',
-      '--no-beautify',
-      '--no-delimited',
-      './src/proto/svga.proto'
-    ],
-    (err) => (err ? reject(err) : resolve())
+const generateJS = () =>
+  new Promise((resolve, reject) =>
+    pbjs.main(
+      [
+        '--target',
+        'static-module',
+        '-w',
+        'es6',
+        '-o',
+        './src/proto/svga.js',
+        '--no-create',
+        '--no-encode',
+        '--no-verify',
+        '--no-convert',
+        '--no-beautify',
+        '--no-delimited',
+        './src/proto/svga.proto',
+      ],
+      (err) => (err ? reject(err) : resolve()),
+    ),
   )
-)
 
 // generate types from generated js
-const generateTypes = () => new Promise((resolve, reject) =>
-  pbts.main(['-o', './src/proto/svga.d.ts', './src/proto/svga.js'], (err) =>
-    err ? reject(err) : resolve()
+const generateTypes = () =>
+  new Promise((resolve, reject) =>
+    pbts.main(['-o', './src/proto/svga.d.ts', './src/proto/svga.js'], (err) =>
+      err ? reject(err) : resolve(),
+    ),
   )
-)
-
 
 /**
  * cli equivalent:
